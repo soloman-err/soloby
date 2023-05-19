f68833b16e2088c0773afb3dde41f3469849bb67
soloman.err <soloman.err@gmail.com>
2023-05-19 11:11:02 +0600

    Authentication method updated

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,9 +1,10 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import {
   GithubAuthProvider,
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
   getAuth,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
   signInWithPopup,
 } from "firebase/auth";
@@ -17,32 +18,59 @@ const googleProvider = new GoogleAuthProvider();
 const githubProvider = new GithubAuthProvider();
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   // Create user with email and password:
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   // Sign in with email & password:
   const emailPassSignIn = (email, password) => {
+    setLoading(true);
+
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   // Sign in with Google:
   const googleSignIn = () => {
+    setLoading(true);
+
     return signInWithPopup(auth, googleProvider);
   };
 
   // Sign in with GitHub:
   const githubSignIn = () => {
+    setLoading(true);
+
     return signInWithPopup(auth, githubProvider);
+  };
+
+  // User observer:
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (loggedUser) => {
+      setUser(loggedUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  // User exit:
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
   };
 
   // Exportable auth info:
   const authInfo = {
+    user,
     createUser,
     googleSignIn,
     githubSignIn,
     emailPassSignIn,
+    logOut,
   };
   return (
     <div>
