b89e967a1fa8b5178d6dc2e701829bf5f9d291ca
soloman.err <soloman.err@gmail.com>
2023-05-19 07:44:04 +0600

    Error page updated with homepage routing

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,15 +1,23 @@
 import { Controls, Player } from "@lottiefiles/react-lottie-player";
 import React from "react";
+import { FaArrowLeft, FaLongArrowAltLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
+    <div className="">
       <Player
         autoplay
         loop
         src="https://lottie.host/1f61cfb1-d07f-47fd-aa89-158578701020/2Vm1VXMJ9F.json"
         className="h-screen"
       ></Player>
+      <div className="-translate-y-5">
+        <Link to="/" className="flex items-center justify-center gap-1">
+          <FaLongArrowAltLeft />
+          <p className="font-bold">Back to home</p>
+        </Link>
+      </div>
     </div>
   );
 };
