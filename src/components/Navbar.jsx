955034148e6ca1391dfe2e0536136bec306fb713
soloman.err <soloman.err@gmail.com>
2023-05-20 18:21:53 +0600

    navbar updated with implementing responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../AuthProvider/AuthProvider";
+import { FaBars, FaCross, FaHamburger } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
 
 const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Navbar toggle:
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
 
   //User logout:
   const handleLogout = () => {
@@ -11,13 +19,13 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="flex justify-between items-center py-2">
+    <nav className="flex flex-row-reverse md:flex-row items-center justify-between md:items-center p-3">
       <Link to="/">
         <h1 className="custom-title text-3xl font-bold">soloby</h1>
       </Link>
       <ul
         id="navul"
-        className="flex font-bold space-x-2 uppercase divide-black"
+        className="hidden md:flex flex-col md:flex-row font-bold md:space-x-2 uppercase divide-black"
       >
         <li>
           <a href="/allToys">All Toys</a>
@@ -37,7 +45,38 @@ const Navbar = () => {
         </li>
       </ul>
 
-      <div className="space-x-2">
+      <div className="md:hidden" onClick={toggleNav}>
+        {isOpen ? <ImCross size={20} /> : <FaBars size={20} />}
+      </div>
+      {isOpen && (
+        <aside
+          className={`bg-gray-200 fixed top-12 p-2 left-0 h-full w-56 transition-transform duration-500 ease-in-out z-50 flex`}
+        >
+          <ul
+            id="navul"
+            className="flex flex-col font-bold md:space-x-0 space-y-1 uppercase"
+          >
+            <li>
+              <a href="/allToys">All Toys</a>
+            </li>
+            {user && (
+              <>
+                <li>
+                  <a href="/myToys">My Toys</a>
+                </li>
+                <li>
+                  <a href="/addAToy">Add a Toy</a>
+                </li>
+              </>
+            )}
+            <li>
+              <a href="/blog">Blog</a>
+            </li>
+          </ul>
+        </aside>
+      )}
+
+      <div className="space-x-2 hidden md:static md:flex">
         {user ? (
           <div className="flex items-center gap-2">
             <img className="w-10 h-10 rounded-lg" src={user.photoURL} alt="" />
